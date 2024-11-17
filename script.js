5149940d150c940eafddde3ffe11c6e01aaa01fc
JKaur2716 <jaspreet814kaur@gmail.com>
2024-11-17 13:29:18 -0700

    Footer set for each page

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("JavaScript is linked!");
+
+// Footer
+
+const footer = document.getElementById("footer");
+const currentYear = new Date().getFullYear();
+footer.innerHTML = `&copy; Jaspreet Kaur, ${currentYear}`;
+
+console.log("Today's date is:", new Date());
+
+
