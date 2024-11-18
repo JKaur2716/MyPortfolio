a066388920dda51eb185ab1af7caaf0bdd124e3d
JKaur2716 <jaspreet814kaur@gmail.com>
2024-11-18 00:12:29 -0700

    Troubleshooted greeting problem

--- script.js
+++ script.js
@@ -9,3 +9,23 @@ footer.innerHTML = `&copy; Jaspreet Kaur, ${currentYear}`;
 console.log("Today's date is:", new Date());
 
 
+
+
+const greeting = document.getElementById("greeting");
+const hours = new Date().getHours();
+
+let message = "";
+let className = "";
+
+if (hours < 12) {
+    message = "Good morning!";
+    className = "morning";
+} else if (hours < 17) {
+    message = "Good afternoon!";
+    className = "afternoon";
+} else {
+    message = "Good evening!"; 
+    className = "evening";    
+}
+greeting.textContent = message; 
+greeting.className = className; 
