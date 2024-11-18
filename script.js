3341697174737778cd982bd48d6dca3348e81f14
JKaur2716 <jaspreet814kaur@gmail.com>
2024-11-18 01:55:28 -0700

    List added, debugging complete

--- script.js
+++ script.js
@@ -32,15 +32,52 @@ greeting.className = className;
 
 // Button 
 
+// Button 
 const alertButton = document.getElementById("btn-alert");
-alertButton.addEventListener("click", () => {
-    alert("Hello! I hope you're doing well :)");
-});
+if (alertButton) {
+    alertButton.addEventListener("click", () => {
+        alert("Hello! I hope you're doing well :)");
+    });
 
-alertButton.addEventListener("mouseover", () => {
-    alertButton.innerText = "Good! Now, click!";
-});
+    alertButton.addEventListener("mouseover", () => {
+        alertButton.innerText = "Good! Now, click!";
+    });
 
-alertButton.addEventListener("mouseout", () => {
-    alertButton.innerText = "Click me";
-});
+    alertButton.addEventListener("mouseout", () => {
+        alertButton.innerText = "Click me";
+    });
+} else {
+    console.error("Button with id 'btn-alert' not found.");
+}
+
+// List of my favorite hiking spots on about me page
+const hikingPlaces = [
+    { name: "Johnston Canyon", url: "https://www.banfflakelouise.com/johnston-canyon" },
+    { name: "Jasper National Park", url: "https://www.jasper.travel/" },
+    { name: "Lake Louise", url: "https://www.banfflakelouise.com/lake-louise" },
+    { name: "Kananaskis Country", url: "https://kananaskis.com/" },
+    { name: "Rawson Lake", url: "https://www.banfflakelouise.com/rawson-lake" } // Updated to Rawson Lake
+];
+
+// Get the ordered list element by ID
+const placesList = document.getElementById("hikingPlaces");
+console.log(placesList); // checking
+
+if (placesList) {
+    for (let i = 0; i < hikingPlaces.length; i++) {
+        console.log("Adding list item:", hikingPlaces[i].name); // Debugging line
+        const listItem = document.createElement("li");
+        const link = document.createElement("a");
+
+        link.href = hikingPlaces[i].url;
+        link.textContent = hikingPlaces[i].name;
+
+        // Links should open in a separate tab
+        link.target = "_blank"; 
+
+        listItem.appendChild(link);
+        placesList.appendChild(listItem);
+    }
+} else {
+    console.error("Element with id 'hikingPlaces' not found.");
+}
